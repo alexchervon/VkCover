2e009432e6e015f2f870844892721025973460d2
Alex Chervon <chervon@cit-psu.ru>
2018-07-01 22:37:27 +0500

    Update changer.py

--- VKHeaderChanger/changer.py
+++ VKHeaderChanger/changer.py
@@ -1,13 +1,24 @@
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
+import requests
+import urllib.parse
+
+# Ваш токен
+token = ""
 
 
 # Настройки текста
 font = ImageFont.truetype('OpenSans-Regular.ttf',25)
 text_color = (255,255,255)
+
+# Выбор изображения на которое будем добавлять текст
 img = Image.open('image.png')
 
+# Настройка шапки
+CONST_WIDTH = 1590 #Размер width
+CONST_HEIGHT = 400 #Размер height
+CONST_NAME = "header" #Название фото-шапки на выходе
 
 # Ф-я создания объекта фото
 def insertText(text,position):
@@ -16,11 +27,51 @@ def insertText(text,position):
 
 
 # Вписываем три текста
-insertText("20",(1113, 90))
-insertText("90",(1140, 157))
-insertText("13",(1217, 234))
-
+insertText("24",(1113, 90))
+insertText("97",(1140, 157))
+insertText("17",(1217, 234))
 
 
 # Рендерим
-img.save('header.png')
+img.save(CONST_NAME+'.png')
+
+
+# Общая Ф-я для отпрвки запросов
+def apiVKRequests(method,param):
+    return requests.request('POST', 'https://api.vk.com/method/'+method+'?'+urllib.parse.urlencode(param)+'&access_token='+token).json()
+
+
+# Замена шапки
+def setPhoto(hash,photo):
+    param = {
+        'hash': hash,
+        'photo':photo,
+        'v': 5.80
+    }
+
+    if 'response' in apiVKRequests('photos.saveOwnerCoverPhoto',param):
+        print('HEADER CHANGED')
+    else: print('error setPhoto')
+
+
+# Получение url для загрузки фото
+def uploadServer():
+    param = {
+        'group_id':138003925,
+        'v':5.80,
+        'crop_x2':CONST_WIDTH,
+        'crop_y2':CONST_HEIGHT
+    }
+
+    getUrl = apiVKRequests('photos.getOwnerCoverPhotoUploadServer',param)
+    if 'response' in getUrl:
+        file = {'file': open(CONST_NAME+'.png', 'rb')}
+        sendPhoto = requests.post(getUrl['response']['upload_url'], files=file).json()
+        hash = sendPhoto['hash']
+        photo = sendPhoto['photo']
+        setPhoto(hash, photo)
+    else: print('error uploadServer',set)
+
+
+
+uploadServer()
